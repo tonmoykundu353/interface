12bfe567a547a5ce99085a843df0d1fd63faef87
Your Name <you@example.com>
2024-01-22 15:59:03 +0600

    4th modification

--- code.cpp
+++ code.cpp
@@ -15,3 +15,30 @@ public:
         cout << "Inside Square::draw() method." << endl;
     }
 };
+class ShapeFactory {
+private:
+    static ShapeFactory* instance; 
+    
+    ShapeFactory() {}
+
+public:
+    
+    static ShapeFactory* getInstance() {
+        if (instance == nullptr) {
+            instance = new ShapeFactory();
+        }
+        return instance;
+    }
+
+    Shape* getShape(const string& shapeType) {
+        if (shapeType.empty()) {
+            return nullptr;
+        }
+        if (shapeType == "RECTANGLE") {
+            return new Rectangle();
+        } else if (shapeType == "SQUARE") {
+            return new Square();
+        }
+        return nullptr;
+    }
+};
